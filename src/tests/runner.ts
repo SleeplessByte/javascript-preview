169e199daeef0e3b7900b1d8564a53f5fd4947f9
Derk-Jan Karrenbeld <derk-jan+github@karrenbeld.info>
2020-04-20 01:31:31 +0200

    Add better equality

--- src/tests/runner.ts
+++ src/tests/runner.ts
@@ -65,6 +65,7 @@ try {
   }
   */
 
+  /*
  function esm2(templateStrings: TemplateStringsArray, ...substitutions: string[]) {
   let js = templateStrings.raw[0];
   for (let i=0; i<substitutions.length; i++) {
@@ -72,6 +73,7 @@ try {
   }
   return 'data:text/javascript;base64,' + btoa(js);
 }
+*/
 
 const esm = ({ raw }: TemplateStringsArray, ...vals: string[]) => URL.createObjectURL(new Blob([String.raw({ raw } as any, ...vals)], { type: 'text/javascript' }));
 
@@ -168,20 +170,170 @@ function promise(p) {
 class AssertionFailed extends Error {
 }
 
+function eq(a, b, aStack = [], bStack = [], customTesters = []) {
+  var result = true;
+
+  if (a instanceof Error && b instanceof Error) {
+    return a.message == b.message;
+  }
+
+  if (Object.is(a, b)) {
+    return true;
+  }
+  // A strict comparison is necessary because \`null == undefined\`.
+  if (a === null || b === null) {
+    return a === b;
+  }
+
+  var className = Object.prototype.toString.call(a);
+  if (className != Object.prototype.toString.call(b)) {
+    return false;
+  }
+
+  switch (className) {
+    case '[object Boolean]':
+    case '[object String]':
+    case '[object Number]':
+      if (typeof a !== typeof b) {
+        // One is a primitive, one a \`new Primitive()\`
+        return false;
+      } else if (typeof a !== 'object' && typeof b !== 'object') {
+        // both are proper primitives
+        return Object.is(a, b);
+      } else {
+        // both are \`new Primitive()\`s
+        return Object.is(a.valueOf(), b.valueOf());
+      }
+    case '[object Date]':
+      // Coerce dates to numeric primitive values. Dates are compared by their
+      // millisecond representations. Note that invalid dates with millisecond representations
+      // of \`NaN\` are not equivalent.
+      return +a == +b;
+    // RegExps are compared by their source patterns and flags.
+    case '[object RegExp]':
+      return a.source === b.source && a.flags === b.flags;
+  }
+  if (typeof a !== 'object' || typeof b !== 'object') {
+    return false;
+  }
+
+  // Add the first object to the stack of traversed objects.
+  aStack.push(a);
+  bStack.push(b);
+  var size = 0;
+  // Recursively compare objects and arrays.
+  // Compare array lengths to determine if a deep comparison is necessary.
+  if (className == '[object Array]') {
+    size = a.length;
+    if (size !== b.length) {
+      return false;
+    }
+
+    while (size--) {
+      result = eq(a[size], b[size], aStack, bStack, hasKey);
+      if (!result) {
+        return false;
+      }
+    }
+  }
+
+  // Deep compare objects.
+  var aKeys = keys(a, className == '[object Array]', hasKey),
+    key;
+  size = aKeys.length;
+
+  // Ensure that both objects contain the same number of properties before comparing deep equality.
+  if (keys(b, className == '[object Array]', hasKey).length !== size) {
+    return false;
+  }
+
+  while (size--) {
+    key = aKeys[size];
+
+    // Deep compare each member
+    result =
+      hasKey(b, key) &&
+      eq(a[key], b[key], aStack, bStack, customTesters, hasKey);
+
+    if (!result) {
+      return false;
+    }
+  }
+  // Remove the first object from the stack of traversed objects.
+  aStack.pop();
+  bStack.pop();
+
+  return result;
+}
+
+function keys(
+  obj,
+  isArray,
+  hasKey
+) {
+  var allKeys = (function(o) {
+    var keys = [];
+    for (var key in o) {
+      if (hasKey(o, key)) {
+        keys.push(key);
+      }
+    }
+    return keys.concat(
+      (Object.getOwnPropertySymbols(o)).filter(
+        symbol =>
+          (Object.getOwnPropertyDescriptor(o, symbol))
+            .enumerable,
+      ),
+    );
+  })(obj);
+
+  if (!isArray) {
+    return allKeys;
+  }
+
+  var extraKeys = [];
+  if (allKeys.length === 0) {
+    return allKeys;
+  }
+
+  for (var x = 0; x < allKeys.length; x++) {
+    if (typeof allKeys[x] === 'symbol' || !allKeys[x].match(/^[0-9]+$/)) {
+      extraKeys.push(allKeys[x]);
+    }
+  }
+
+  return extraKeys;
+}
+
+function hasKey(obj, key) {
+  return Object.prototype.hasOwnProperty.call(obj, key);
+}
+
 function expect(value) {
+
+  function promiseToBe(x) {
+    return promise(
+      value.then(
+        (resolved) => {
+          if (!eq(x, resolved)) {
+            throw new AssertionFailed(\`Expected \${JSON.stringify(resolved, undefined, 2)} to be \${x}\`)
+          }
+        }
+      )
+    )
+  }
+
+  function toBe(x) {
+    if (!eq(x, value)) {
+      throw new AssertionFailed(\`Expected \${value} to be \${x}\`)
+    }
+  }
+
   return {
     resolves: {
-      toBe(x) {
-        return promise(
-          value.then(
-            (resolved) => {
-              if (x !== resolved) {
-                throw new AssertionFailed(\`Expected \${JSON.stringify(resolved, undefined, 2)} to be \${x}\`)
-              }
-            }
-          )
-        )
-      }
+      toBe: promiseToBe,
+      toEqual: promiseToBe,
+      toStrictEqual: promiseToBe
     },
     rejects: {
       toThrow(x) {
@@ -202,17 +354,9 @@ function expect(value) {
 
       throw new AssertionFailed(\`Expected \${value} to be close to \${x}\`)
     },
-    toBe(x) {
-      if (x !== value) {
-        throw new AssertionFailed(\`Expected \${value} to be \${x}\`)
-      }
-    },
-    toEqual(x) {
-      // eslint-disable-next-line eqeqeq
-      if (x != value) {
-        throw new AssertionFailed(\`Expected \${value} to equal \${x}\`)
-      }
-    }
+    toBe: toBe,
+    toEqual: toBe,
+    toStrictEqual: toBe
   }
 }
 
